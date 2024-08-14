9150a2388d4878df0acdb353940ef2c108e20b9e
Enzo T Appi <enzoappi.tiriba@gmail.com>
2024-08-14 09:51:21 -0300

    ajustes para jogo a dois

--- tictactoe.py
+++ tictactoe.py
@@ -5,27 +5,11 @@ import random
 
 class Board:
     def __init__(self):
-        self.player = None
-        self.computer_player = None
+        self.player = {1: "X", 2: "O"}
         self.areas={1: " ", 2: " ", 3: " ", 4: " ", 5: " ", 6: " ", 7: " ", 8: " ", 9: " "}
 
-    def set_players_type(self):
-        r = None
-        while r not in(1, 2):
-            try:
-                r = int(input("""
-                Which kind of player you want to be? 
-                
-                1 - X
-               
-                2 - O
-               
-                type you choice: """))
-            except Exception as e:
-                tipo_erro = type(e).__name__
-                print(f"\nERROR: {tipo_erro}!\nPlease type a valid option.\n")
-        self.player = "X" if r==1 else "O"
-        self.computer_player = "O" if r==1 else "X"
+    def get_player(self, n):
+        return self.player[n]
 
     def set_areas(self, n, player):
         self.areas[n] = player
@@ -33,35 +17,24 @@ class Board:
     def get_areas(self, n):
         return self.areas.get(n)
 
-    def play_choices(self):
-        while True:
-            players_play_choice = None
-            computers_play_choice = None
-            while players_play_choice not in (1, 9):
-                try:               
-                    players_play_choice = int(input("What's your play: "))
-                    if self.get_areas(players_play_choice) == self.player or self.get_areas(players_play_choice) != " ":
-                            print("\nThis Area has already been chosen! Please select another one.\n")
-                            players_play_choice = None
-                    else:
-                            self.set_areas(players_play_choice, self.player)
-                            break
-                except Exception as e:
-                    tipo_erro = type(e).__name__
-                    print(f"\nERROR: {tipo_erro}!\nPlease type a valid option.\n")    
-            self.show_map(True)
-            if self.test_victory(): break
-            time.sleep(0.8)
-            while True:
-                computers_play_choice = random.randint(1, 9)
-                if self.get_areas(computers_play_choice) == self.computer_player or self.get_areas(computers_play_choice) != " ":
-                    computers_play_choice = random.randint(1, 9)
+    def play_choices(self, player):
+        players_play_choice = None
+        while players_play_choice not in (1, 9):
+            try:               
+                players_play_choice = int(input(f"Player '{self.get_player(player)}' what's your play: "))
+                if self.get_areas(players_play_choice) == self.get_player(player) or self.get_areas(players_play_choice) != " ":
+                        print("\nThis Area has already been chosen! Please select another one.\n")
+                        players_play_choice = None
                 else:
-                    self.set_areas(computers_play_choice, self.computer_player)
-                    break
-            self.show_map(True)
-            if self.test_victory(): break
-
+                        self.set_areas(players_play_choice, self.get_player(player))
+                        break
+            except Exception as e:
+                tipo_erro = type(e).__name__
+                print(f"\nERROR: {tipo_erro}!\nPlease type a valid option.\n")    
+        self.show_map(True)
+        if not self.test_victory(): 
+            self.play_choices(2 if player == 1 else 1)
+    
     def test_victory(self):
         victory_sequencies = [
             (1, 2, 3), (1, 4, 7), (1, 5, 9), 
@@ -98,10 +71,27 @@ class Board:
         print("\n")
 
 def main():
-    board = Board()
-    board.show_map(False)
-    board.set_players_type()
-    board.play_choices()
+    while True:
+        board = Board()
+        board.show_map(False)
+        board.play_choices(1)
+        try:
+            r = int(input("""
+            Type: 
+            
+                0 - Continue
+                          
+                        OR
+                          
+                1 - Exit
+            
+            type you choice: """))
+            if r == 1:
+                print("\nSee you later! Thanks for playing Tic Tac Toe's Game!\n")
+                break
+        except Exception as e:
+            tipo_erro = type(e).__name__
+            print(f"\nERROR: {tipo_erro}!\nPlease type a valid option.\n")
 
 #Play-time
 main()
